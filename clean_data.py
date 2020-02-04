17194c61d92f480ad7081a57e70ca911891aa7c7
Jared Keil <jared@Jareds-MacBook-Pro.local>
2020-02-03 21:31:32 -0800

    fixing pipeline bugs

--- clean_data.py
+++ clean_data.py
@@ -1,12 +1,23 @@
 import numpy as np
 import pandas as pd
 from src.feature_extraction import vector_merge
+import boto3
+import os
 
 
-# Loading labeled data
+# read in labelled csv with file names
 df = pd.read_csv('data/train_labels.csv').set_index('fname')
 # dropping unneeded columns, corrupted files
 df.drop(['license', 'freesound_id'], axis=1, inplace=True)
 df.drop(['f76181c4.wav', '77b925c2.wav', '6a1f682a.wav', 'c7db12aa.wav', '7752cc8a.wav', '1d44b0bd.wav'], inplace=True)
 df = df.reset_index()
+
+
+if __name__ == "__main__":
+    s3_client = boto3.client('s3')
+    bucket_name = 'jarednewstudy'
+    s3_folder = 'audio_train/'
+    df2 = vector_merge(df, s3_client, bucket_name, s3_folder, sample_size=5000)
+    df2.to_csv('data/vectorized.csv')
+    os.system("say 'complete'")
 
--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -3,14 +3,20 @@ import pandas as pd
 import boto3
 from io import BytesIO
 import librosa
+from botocore.exceptions import ClientError
 
 
-def call_s3(fname, folder='audio_train/'):
+def call_s3(s3_client, bucket_name, fname, folder='audio_train/'):
     """Call S3 instance to retrieve data from .wav file(or other format).
     Assumes file is in folder name path"""
     path = folder + fname
-    response = s3_client.get_object(Bucket=bucket_name, Key=path,)
-    data = BytesIO(response['Body'].read())
+    try:
+        response = s3_client.get_object(Bucket=bucket_name, Key=path)
+    except ClientError as ex:
+        if ex.response['Error']['Code'] == 'NoSuchKey':
+            return dict()
+
+    data = BytesIO(response['Body'].read()) 
     return data
 
 
@@ -47,16 +53,17 @@ def audio_vectorize(fname, data):
     return pd.Series(vector_dict)
 
 
-def vector_merge(df, s3_folder = 'audio_train/', sample_size=200):
+def vector_merge(df, s3_client, bucket_name, s3_folder = 'audio_train/', sample_size=200):
     """
-    Helper function for merging returned feature/vector dictionaries for multiple files,
-    (number defined by sample_size) onto labelled dataframe, based on file names
+    Helper function for merging returned feature/vector dictionaries for multiple files in an S3 bucket,
+    (number defined by sample_size) onto labelled dataframe, based on file names.
     """
     vectors = []
     for i, fname in enumerate(df.loc[:sample_size-1, 'fname']):
-        data = call_s3(fname, s3_folder)
+        data = call_s3(s3_client, bucket_name, fname, s3_folder)
         vec = audio_vectorize(fname, data)
         vectors.append(vec)
+        print(i, ' ', fname)
 
     vec_df = pd.concat(vectors, axis=1, sort=True).T
     df2 = pd.merge(df, vec_df, how='inner', on='fname')
