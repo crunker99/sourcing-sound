e7ee67a259a73468d9c908000b05e9333ac878ce
Ubuntu <ubuntu@ip-172-31-22-106.ec2.internal>
2020-02-17 07:59:11 +0000

    update pre_process more

--- pre_process.py
+++ pre_process.py
@@ -1,9 +1,11 @@
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
+from scipy.io import wavfile
 
 def get_single_labels(TOT='train', subset={}):
     df = pd.read_csv('data/{}/labels.csv'.format(TOT))
+    df.set_index('fname', inplace=True)
     df['labels'] = df['labels'].apply(lambda x: x.split(','))
     df = df[df['labels'].map(len) == 1]
     df['labels'] = df['labels'].apply(lambda x: ''.join(x))
@@ -15,6 +17,7 @@ def get_single_labels(TOT='train', subset={}):
             rate, signal = wavfile.read('audio/{}/{}'.format(TOT, f))
         except ValueError:
             df.drop(f, 0, inplace=True)
+    df.reset_index(inplace=True)
     return df
 
 if __name__ == "__main__":
@@ -22,6 +25,6 @@ if __name__ == "__main__":
     roadsound = {'Accelerating_and_revving_and_vroom', 'Bicycle_bell', 'Bus', 
                     'Car_passing_by' 'Motorcycle', 'Race_car_and_auto_racing', 
                     'Skateboard', 'Traffic_noise_and_roadway_noise',}
-    df = get_single_labels(TOT, roadsounds)
+    df = get_single_labels(TOT, roadsound)
     df.to_csv('data/{}/roadsound_labels.csv'.format(TOT))
 
