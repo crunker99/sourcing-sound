2d2876dc1b922f45dcdd1336b4cce15fba8358d6
Jared Keil <jared@Jareds-MacBook-Pro.local>
2020-02-22 23:05:41 -0800

    debug config save

--- buildfeats.py
+++ buildfeats.py
@@ -2,6 +2,7 @@ import os
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 from scipy.io import wavfile
 from python_speech_features import mfcc
 import librosa
@@ -25,21 +26,20 @@ def build_rand_feat(df, split):
     tmp = check_data()
     if not tmp:
         tmp = Config()
-        tmp.data = [None, None, None, None]
+        tmp.data = defaultdict(list)
     if split == 'train' and not tmp.data[0] is None:
             return tmp.data[0], tmp.data[1]
     elif split == 'val' and not tmp.data[2] is None:
             return tmp.data[2], tmp.data[3]
     # config.data = [None, None, None, None]
-    
-    X = []
-    y = []
     n_samples = 1 * int(df['length'].sum() / 0.1)
     classes = list(np.unique(df.labels))
     class_dist = df.groupby(['labels'])['length'].mean()
     prob_dist = class_dist / class_dist.sum()
-
     _min, _max = float('inf'), -float('inf')
+
+    X = []
+    y = []
     print('Building features for '+split)
     for _ in tqdm(range(n_samples)):
         rand_class = np.random.choice(classes, p=prob_dist)
@@ -47,9 +47,7 @@ def build_rand_feat(df, split):
         rate, wav = wavfile.read('clean/'+file)
         rand_index = np.random.randint(0, wav.shape[0] - config.step)
         sample = wav[rand_index:rand_index + config.step]
-        # if config.pca == True:
-        #     pca = PCA(n_components=20, random_state=1738)
-            ##############
+
         if config.feature_type == 'mfccs':
             X_sample = mfcc(sample, rate, numcep=config.nfeat,
                             nfilt=config.nfilt, nfft = config.nfft)
@@ -59,6 +57,7 @@ def build_rand_feat(df, split):
             X_sample = librosa.power_to_db(X_sample)
         elif config.feature_type == 'raw':
             X_sample = sample
+
         _min = min(np.amin(X_sample), _min)
         _max = max(np.amax(X_sample), _max)
         X.append(X_sample if config.mode== 'conv' else X_sample.T)
@@ -72,10 +71,13 @@ def build_rand_feat(df, split):
     elif config.mode == 'time':
         X = X.reshape(X.shape[0], X.shape[1], X.shape[2])
     y = to_categorical(y)
+
     if split == 'train':
+        config.data = [0, 0, 0, 0]
         config.data[0], config.data[1] = (X, y)
     elif split == 'val':
-        config.data[2], config.data[3] = (X, y)
+        config.data.append(X)
+        config.data.append(y)
 
     # with open(config.p_path, 'wb') as handle:
     #     pickle.dump(config, handle, protocol=2)
